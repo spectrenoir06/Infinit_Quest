3a46dd4a34bce82893a6c8649692d490677c047c
spectrenoir06 <antoine.doussaud@laposte.net>
2013-12-08 23:00:56 +0100

    add dt to pureLua with socket.gettime()

--- Serveur/pureLua.lua
+++ Serveur/pureLua.lua
@@ -9,25 +9,25 @@
 	udp:settimeout(0)
     udp:setsockname('*', 12345)
 	server = serveur_new()
-		print("serveur start on : "..udp:getsockname())
+	print("serveur start on : "..udp:getsockname())
 	
-	sync = 0
-	sync_dt = 0.05
-	
-
+	sync_dt = 0.075
+	temp = 0
+	dt=0
 
 while 1 do
-	
-	sync = sync + 0.1	
+	temp = temp + dt
+	sync = socket.gettime()
 	local udp_data, msg_or_ip, port_or_nil = udp:receivefrom()
-	
 
 	if udp_data then
 		server:receive(udp_data, msg_or_ip, port_or_nil)
-	else
-		server:update(1)
-		sync = sync-sync_dt
-		socket.sleep(0.05)
 	end
-
+	
+	if temp>sync_dt then
+		server:update(1)
+		temp = temp-sync_dt
+	end
+	dt = socket.gettime()-sync
+	
 end
