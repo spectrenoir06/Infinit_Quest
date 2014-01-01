d9852919d4f742157fb9078b3b785e5bd5b9a352
spectrenoir06 <antoine.doussaud@laposte.net>
2014-01-01 21:01:28 +0100

    Revert "maj  love 9.0 bug json"
    This reverts commit 0ce8720298b725905ff5fc89120099162ef82327.

--- Client/fonction/option.lua
+++ Client/fonction/option.lua
@@ -1,8 +1,8 @@
 function load_option()
 	resolution = 64  -- taile des tiles
 	
-    icone=( love.image.newImageData( "icone.png" ) )  -- icone de la fenetre
-    love.window.setIcon(icone)
+    icone=( love.graphics.newImage( "icone.png" ) )  -- icone de la fenetre
+    love.graphics.setIcon(icone)
 
 	if love.keyboard.isDown( "up" ) and love.keyboard.isDown( "down" ) then
         mobile=true
